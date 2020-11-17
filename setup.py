d7606d328f2679cb1abfa20eb88ef1ab5694d682
major seitan <majorseitan@gmail.com>
2020-11-17 15:58:14 +0000

    fix:rel is a int

--- setup.py
+++ setup.py
@@ -12,14 +12,15 @@ setup(name='pheweb_colocalization',
       tests_require=['pytest', 'tox', 'pytest-cov', ],
       install_requires=[
           'Flask>=0.12',
-          'finngen_common_data_model@git+https://github.com/FINNGEN/finngen-common-data-model.git@5c07c033bf5d96513d0095a69eef17d5b1e093ad#egg=finngen_common_data_model',
+          'finngen_common_data_model@git+https://github.com/FINNGEN/finngen-common-data-model.git@e73e6e19a6bfda2e3e893e3dd5252aaf66ccb8f5#egg=finngen_common_data_model',
           'attrs>=19.3.0',
           'SQLAlchemy>=1.3.18',
           'PyMySQL>=0.10.1',
           'pytest>=5.4.3',
           'mysqlclient>=2.0.1',
       ],
-      dependency_links=['https://github.com/FINNGEN/finngen-common-data-model.git@5c07c033bf5d96513d0095a69eef17d5b1e093ad#egg=finngen_common_data_model'],
+      dependency_links=[
+          'https://github.com/FINNGEN/finngen-common-data-model.git@e73e6e19a6bfda2e3e893e3dd5252aaf66ccb8f5#egg=finngen_common_data_model'],
       extras_require={
           'dev': ['pytest>=6.1.2', 'pytest-cov>=2.10.1', ],
       }
