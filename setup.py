8a88a916fd654d8c5c8778f9108c8d9b1188470d
major seitan <majorsetian@gmail.com>
2021-12-06 17:04:19 +0000

    bump version of commons

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='pheweb_colocalization',
       tests_require=['pytest', 'tox', 'pytest-cov', ],
       install_requires=[
           'Flask>=0.12',
-          'finngen_common_data_model@git+https://github.com/FINNGEN/finngen-common-data-model.git@e73e6e19a6bfda2e3e893e3dd5252aaf66ccb8f5#egg=finngen_common_data_model',
+          'finngen_common_data_model@git+https://github.com/FINNGEN/finngen-common-data-model.git@7a3d457cdcccdbd7e8be173259150ec98e2737ad#egg=finngen_common_data_model',
           'attrs>=19.3.0',
           'SQLAlchemy>=1.3.18',
           'PyMySQL>=0.10.1',
@@ -20,7 +20,7 @@ setup(name='pheweb_colocalization',
           'mysqlclient>=2.0.1',
       ],
       dependency_links=[
-          'https://github.com/FINNGEN/finngen-common-data-model.git@e73e6e19a6bfda2e3e893e3dd5252aaf66ccb8f5#egg=finngen_common_data_model'],
+          'https://github.com/FINNGEN/finngen-common-data-model.git@7a3d457cdcccdbd7e8be173259150ec98e2737ad#egg=finngen_common_data_model'],
       extras_require={
           'dev': ['pytest>=6.1.2', 'pytest-cov>=2.10.1', ],
       }
