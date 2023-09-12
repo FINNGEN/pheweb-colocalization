3f61c8fdec184d85fcbd83840d641365519482fe
Anastasia Shcherban <anastasia.shcherban@helsinki.fi>
2023-09-12 12:53:16 +0300

    Updated setup to refert to use updated coloc schema from fg-common-data-model repo.

--- setup.py
+++ setup.py
@@ -11,16 +11,16 @@ setup(name='pheweb_colocalization',
       package_dir={ 'pheweb_colocalization': 'pheweb_colocalization' },
       tests_require=['pytest', 'tox', 'pytest-cov', ],
       install_requires=[
-          'Flask>=0.12',
-          'finngen_common_data_model@git+https://github.com/FINNGEN/finngen-common-data-model.git@085519962cd4742361a86c23710ea3cc341f2c98#egg=finngen_common_data_model',
+          'Flask~=2.2.0',
+          'finngen_common_data_model@git+https://github.com/FINNGEN/finngen-common-data-model.git@6f7631bb28a678d23dff0e8120a6c6bb6f3c15e0#egg=finngen_common_data_model',
           'attrs>=19.3.0',
-          'SQLAlchemy>=1.3.18',
+          'SQLAlchemy~=1.3.18',
           'PyMySQL>=0.10.1',
           'pytest>=5.4.3',
-          'mysqlclient>=2.0.1',
+          'mysqlclient~=2.0.1',
       ],
       dependency_links=[
-          'https://github.com/FINNGEN/finngen-common-data-model.git@7a3d457cdcccdbd7e8be173259150ec98e2737ad#egg=finngen_common_data_model'],
+          'https://github.com/FINNGEN/finngen-common-data-model.git@6f7631bb28a678d23dff0e8120a6c6bb6f3c15e0#egg=finngen_common_data_model'],
       extras_require={
           'dev': ['pytest>=6.1.2', 'pytest-cov>=2.10.1', ],
       }
