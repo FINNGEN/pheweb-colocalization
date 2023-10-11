028b94ed7612eb127021ba370b8d9fa365966583
Anastasia Shcherban <anastasia.shcherban@helsinki.fi>
2023-10-11 16:00:53 +0300

    Udpated setup

--- setup.py
+++ setup.py
@@ -12,15 +12,12 @@ setup(name='pheweb_colocalization',
       tests_require=['pytest', 'tox', 'pytest-cov', ],
       install_requires=[
           'Flask~=2.0.3',
-          'finngen_common_data_model@git+https://github.com/FINNGEN/finngen-common-data-model.git@1696ee5e38d93ba77327a45ab4bc83b88d13d52e#egg=finngen_common_data_model',
           'attrs>=19.3.0',
           'SQLAlchemy~=1.3.19',
           'PyMySQL>=0.10.1',
           'pytest>=5.4.3',
           'mysqlclient~=2.0.1',
       ],
-      dependency_links=[
-          'https://github.com/FINNGEN/finngen-common-data-model.git@1696ee5e38d93ba77327a45ab4bc83b88d13d52e#egg=finngen_common_data_model'],
       extras_require={
           'dev': ['pytest>=6.1.2', 'pytest-cov>=2.10.1', ],
       }
